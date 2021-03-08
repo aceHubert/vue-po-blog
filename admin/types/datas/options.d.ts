4cb61e6016267a5cb2c8b962e5c48c05102c1ce3
aceHubert <yi.xiang@live.cn>
2021-03-08 13:04:40 +0800

    chore: 项目配置和清理
    affects: @po/admin

--- admin/types/datas/options.d.ts
+++
@@ -1,7 +0,0 @@
-import { PagerQuery, PagerResponse } from './request';
-
-export interface OptionsApi {
-    getList(query?: any): Promise<any>;
-    create(data: any): Promise<true>;
-  }
-  
--- scripts/genChangelog.js
+++ scripts/genChangelog.js
@@ -1,4 +1,11 @@
-// cp package.json _package.json &&preset=`conventional-commits-detector` && echo $preset && bump=`conventional-recommended-bump -p angular` && echo ${1:-$bump} && npm --no-git-tag-version version ${1:-$bump} &>/dev/null && conventional-changelog -i CHANGELOG.md -s -p ${2:-$preset} && git add CHANGELOG.md package.json package-lock.json && version=`cat package.json` && git commit -m'docs(CHANGELOG): $version' && mv -f _package.json package.json && npm version ${1:-$bump} -m 'chore(release): %s' && git push --follow-tags
+/** cp package.json _package.json &&preset=`conventional-commits-detector` &&
+ * echo $preset && bump=`conventional-recommended-bump -p angular` &&
+ * echo ${1:-$bump} && npm --no-git-tag-version version ${1:-$bump} &>/dev/null &&
+ * conventional-changelog -i CHANGELOG.md -s -p ${2:-$preset} && git add CHANGELOG.md package.json
+ * package-lock.json && version=`cat package.json` && git commit -m'docs(CHANGELOG): $version' &&
+ * mv -f _package.json package.json && npm version ${1:-$bump} -m 'chore(release): %s' &&
+ * git push --follow-tags
+ */
 
 const fs = require('fs');
 const path = require('path');
